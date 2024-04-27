d0165b6b27fc433f2bad88db0cc3c19da70e0824
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2024-04-27 21:41:52 +0600

    Added an basic api for location

---
+++ app/api/location/[name]/route.js
@@ -0,0 +1,7 @@
+import { getLocationByName } from "../location-util";
+
+export async function GET(request, { params }) {
+  const locationData = getLocationByName(params?.name);
+
+  return Response.json(locationData);
+}
