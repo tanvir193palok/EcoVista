78712c30cad7fe36c892332efc1a8f91a95bb4a8
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2024-04-27 20:57:29 +0600

    Initial setup of projects

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+        },
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
       },
     },
   },
