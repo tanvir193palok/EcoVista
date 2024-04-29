2c215d33d83f091a644e6f76a7e9b5a7f18ff565
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2024-04-29 23:09:08 +0600

    Added design update

--- components/AQIComponent.jsx
+++ components/AQIComponent.jsx
@@ -23,123 +23,125 @@ const AQIComponent = async ({ lat, lon }) => {
   };
 
   return (
-    <Card>
-      <h6 className="feature-name">Air Pollution & Quality</h6>
+    <div className="col-span-12 lg:col-span-4 2xl:col-span-6">
+      <div className="card">
+        <h6 className="feature-name">Air Pollution & Quality</h6>
 
-      <div className="mt-3 space-y-2 lg:space-y-3">
-        <div className="flex items-center justify-between gap-4">
-          <div className="flex items-center gap-2 text-sm text-[#CADEE8] lg:text-base">
-            <Image
-              className="max-w-[18px]"
-              src="/assets/icons/icon_air_element.png"
-              alt="icon"
-              width={40}
-              height={40}
-            />
-            Air Quality Index
+        <div className="mt-3 space-y-2 lg:space-y-4">
+          <div className="flex items-center justify-between gap-4">
+            <div className="flex items-center gap-2 text-sm text-[#CADEE8] lg:text-base">
+              <Image
+                className="max-w-[18px]"
+                src="/assets/icons/icon_air_element.png"
+                alt="icon"
+                width={40}
+                height={40}
+              />
+              Air Quality Index
+            </div>
+            <span className="text-right text-sm text-white lg:text-base">
+              {getAQIRating(main?.aqi)}
+            </span>
           </div>
-          <span className="text-right text-sm text-white lg:text-base">
-            {getAQIRating(main?.aqi)}
-          </span>
-        </div>
 
-        <div className="flex items-center justify-between gap-4">
-          <div className="flex items-center gap-2 text-sm text-[#CADEE8] lg:text-base">
-            <Image
-              className="max-w-[18px]"
-              src="/assets/icons/icon_air_element.png"
-              alt="icon"
-              width={40}
-              height={40}
-            />
-            Carbon Monoxide
+          <div className="flex items-center justify-between gap-4">
+            <div className="flex items-center gap-2 text-sm text-[#CADEE8] lg:text-base">
+              <Image
+                className="max-w-[18px]"
+                src="/assets/icons/icon_air_element.png"
+                alt="icon"
+                width={40}
+                height={40}
+              />
+              Carbon Monoxide
+            </div>
+            <span className="text-right text-sm text-white lg:text-base">
+              {components.co} µg/m³
+            </span>
           </div>
-          <span className="text-right text-sm text-white lg:text-base">
-            {components.co} µg/m³
-          </span>
-        </div>
 
-        <div className="flex items-center justify-between gap-4">
-          <div className="flex items-center gap-2 text-sm text-[#CADEE8] lg:text-base">
-            <Image
-              className="max-w-[18px]"
-              src="/assets/icons/icon_air_element.png"
-              alt="icon"
-              width={40}
-              height={40}
-            />
-            Nitric Oxide
+          <div className="flex items-center justify-between gap-4">
+            <div className="flex items-center gap-2 text-sm text-[#CADEE8] lg:text-base">
+              <Image
+                className="max-w-[18px]"
+                src="/assets/icons/icon_air_element.png"
+                alt="icon"
+                width={40}
+                height={40}
+              />
+              Nitric Oxide
+            </div>
+            <span className="text-right text-sm text-white lg:text-base">
+              {components.no} ppm
+            </span>
           </div>
-          <span className="text-right text-sm text-white lg:text-base">
-            {components.no} ppm
-          </span>
-        </div>
 
-        <div className="flex items-center justify-between gap-4">
-          <div className="flex items-center gap-2 text-sm text-[#CADEE8] lg:text-base">
-            <Image
-              className="max-w-[18px]"
-              src="/assets/icons/icon_air_element.png"
-              alt="icon"
-              width={40}
-              height={40}
-            />
-            Nitrogen Dioxide
+          <div className="flex items-center justify-between gap-4">
+            <div className="flex items-center gap-2 text-sm text-[#CADEE8] lg:text-base">
+              <Image
+                className="max-w-[18px]"
+                src="/assets/icons/icon_air_element.png"
+                alt="icon"
+                width={40}
+                height={40}
+              />
+              Nitrogen Dioxide
+            </div>
+            <span className="text-right text-sm text-white lg:text-base">
+              {components.no2} ppm
+            </span>
           </div>
-          <span className="text-right text-sm text-white lg:text-base">
-            {components.no2} ppm
-          </span>
-        </div>
 
-        <div className="flex items-center justify-between gap-4">
-          <div className="flex items-center gap-2 text-sm text-[#CADEE8] lg:text-base">
-            <Image
-              className="max-w-[18px]"
-              src="/assets/icons/icon_air_element.png"
-              alt="icon"
-              width={40}
-              height={40}
-            />
-            Ozone
+          <div className="flex items-center justify-between gap-4">
+            <div className="flex items-center gap-2 text-sm text-[#CADEE8] lg:text-base">
+              <Image
+                className="max-w-[18px]"
+                src="/assets/icons/icon_air_element.png"
+                alt="icon"
+                width={40}
+                height={40}
+              />
+              Ozone
+            </div>
+            <span className="text-right text-sm text-white lg:text-base">
+              {components.o3} µg/m³
+            </span>
           </div>
-          <span className="text-right text-sm text-white lg:text-base">
-            {components.o3} µg/m³
-          </span>
-        </div>
 
-        <div className="flex items-center justify-between gap-4">
-          <div className="flex items-center gap-2 text-sm text-[#CADEE8] lg:text-base">
-            <Image
-              className="max-w-[18px]"
-              src="/assets/icons/icon_air_element.png"
-              alt="icon"
-              width={40}
-              height={40}
-            />
-            Sulfur Dioxide
+          <div className="flex items-center justify-between gap-4">
+            <div className="flex items-center gap-2 text-sm text-[#CADEE8] lg:text-base">
+              <Image
+                className="max-w-[18px]"
+                src="/assets/icons/icon_air_element.png"
+                alt="icon"
+                width={40}
+                height={40}
+              />
+              Sulfur Dioxide
+            </div>
+            <span className="text-right text-sm text-white lg:text-base">
+              {components.so2} ppm
+            </span>
           </div>
-          <span className="text-right text-sm text-white lg:text-base">
-            {components.so2} ppm
-          </span>
-        </div>
 
-        <div className="flex items-center justify-between gap-4">
-          <div className="flex items-center gap-2 text-sm text-[#CADEE8] lg:text-base">
-            <Image
-              className="max-w-[18px]"
-              src="/assets/icons/icon_air_element.png"
-              alt="icon"
-              width={40}
-              height={40}
-            />
-            PM2.5
+          <div className="flex items-center justify-between gap-4">
+            <div className="flex items-center gap-2 text-sm text-[#CADEE8] lg:text-base">
+              <Image
+                className="max-w-[18px]"
+                src="/assets/icons/icon_air_element.png"
+                alt="icon"
+                width={40}
+                height={40}
+              />
+              PM2.5
+            </div>
+            <span className="text-right text-sm text-white lg:text-base">
+              {components.pm2_5} µg/m³
+            </span>
           </div>
-          <span className="text-right text-sm text-white lg:text-base">
-            {components.pm2_5} µg/m³
-          </span>
         </div>
       </div>
-    </Card>
+    </div>
   );
 };
 
--- components/Card.jsx
+++ components/Card.jsx
@@ -1,7 +1,7 @@
 const Card = ({ children }) => {
   return (
     <div className="col-span-12 lg:col-span-4 2xl:col-span-3">
-      <div className="card">{children}</div>
+      <div className="card" >{children}</div>
     </div>
   );
 };
