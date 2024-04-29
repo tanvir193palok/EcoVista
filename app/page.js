48e7d59cf01ec1cd853d802c33ff134040357c84
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2024-04-29 17:47:58 +0600

    Wrapped useSearchParams inside suspense boundary

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import LocationFinder from "@/components/LocationFinder";
-
+import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <LocationFinder />
+    <Suspense>
+      <LocationFinder />
+    </Suspense>
   );
 }
